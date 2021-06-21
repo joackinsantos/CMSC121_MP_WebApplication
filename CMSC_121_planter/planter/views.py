7bc7f781dddb4d0cea53ed38a68776950364e034
karlostuazon <karlostuazon@gmail.com>
2021-06-21 17:03:43 +0800

    Superuser and intial register and login

--- CMSC_121_planter/planter/views.py
+++ CMSC_121_planter/planter/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm 
+
+from . forms import CreateUserForm
 
 # Create your views here.
 
 def home(request):
     return render(request, "planter/home.html")
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'planter/register.html', context)
+
+def loginPage(request):
+    context = {}
+    return render(request, 'planter/login.html', context)
